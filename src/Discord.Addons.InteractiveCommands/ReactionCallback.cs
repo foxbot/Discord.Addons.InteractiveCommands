986047032e9fae7d6aa495f5a3f3840fc9d9b83c
Confruggy <marcel.p99@gmx.de>
2017-02-06 18:21:00 +0100

    Add files via upload

---
+++ src/Discord.Addons.InteractiveCommands/ReactionCallback.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Discord.Addons.ReactionCallbacks
+{
+    public class ReactionCallback
+    {
+        public bool ResumeAfterExecution { get; set; }
+
+        public Func<IUser, Task> Callback { get; set; }
+    }
+}
